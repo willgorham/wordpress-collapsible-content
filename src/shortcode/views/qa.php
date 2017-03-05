ca8aca26bd8f43ef6e01f81f07b59e37abbc7604
Will Gorham <willgorham@gmail.com>
2017-03-05 15:26:54 -0500

    QA and Teaser shortcode HTML

---
+++ src/shortcode/views/qa.php
@@ -0,0 +1,6 @@
+
+<dl class="qa--container">
+  <dt class="qa--question" data-show-icon="<?php echo $attributes['show_icon']; ?>" data-hide-icon="<?php echo $attributes['show_icon']; ?>">
+    <span class="<?php echo $attributes['show_icon']; ?>"></span> <?php esc_html_e( $attributes['question'] ); ?></dt>
+  <dd class="qa--answer"><?php echo $hidden_content; ?></dd>
+</dl>
---
+++ src/shortcode/views/teaser.php
@@ -0,0 +1,6 @@
+
+<div class="teaser--container">
+  <div class="teaser--visible-message" data-show-icon="<?php echo $attributes['show_icon']; ?>" data-hide-icon="<?php echo $attributes['show_icon']; ?>">
+    <span class="<?php echo $attributes['show_icon']; ?>"></span> <?php esc_html_e( $attributes['visible_message'] ); ?></div>
+  <div class="teaser--hidden-content"><?php echo $hidden_content; ?></div>
+</div>
